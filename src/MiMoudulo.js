442869efcdb778a3a030abc00bd2083f7949ae71
lautainfante <lautaroinfanteferrari@gmail.com>
2022-01-10 15:26:59 -0300

    navbar

---
+++ src/MiMoudulo.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+export const MiModulo = () => {
+    return(
+        <div>
+            <h2>Hola Mundo Coder</h2>
+            <br></br>
+            <p>Tutorial React</p>
+        </div>
+    )
+}
