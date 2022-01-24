545ea08c50265db2f29457847e0b37bf96cc547d
lautainfante <lautaroinfanteferrari@gmail.com>
2022-01-24 14:20:00 -0300

    entrega de la clase 'promise,asincronia y MAP'

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import "./App.css";
 import Navbar from './components/Navbar';
 import Cards from './components/Cards';
 import Cont from './components/ItemCounter' ;
+import Item from "./components/item/item";
+import { useEffect } from "react";
+
+
+
+const PRODUCTS =[
+  {id:0, name:"Horno Electrico", descripcion:"descripcion del objeto", price:1500},
+  {id:1, name:"Estufa Electrica", descripcion:"descripcion del objeto", price:6000},
+  {id:2, name:"Heladera", descripcion:"descripcion del objeto", price:50.000},
+];
+
 
 function App() {
+  useEffect(() =>{
+    const getProducts = () =>{
+      return new Promise((resolve,reject)=>{
+        setTimeout(() => resolve(PRODUCTS),reject(new Error("Se cayo el sistema")),4000);
+      });
+    };
+  });
   return <div className="App">
     <header>
       <nav>
@@ -11,8 +29,9 @@ function App() {
       </nav>
     </header>
     <main>
-      <Cards />
-      <Cards/>
+      <Item key={PRODUCTS.id} Product={PRODUCTS[0]} />
+      <Item key={PRODUCTS.id} Product={PRODUCTS[1]} />
+      <Item key={PRODUCTS.id} Product={PRODUCTS[2]} />
       <Cont />
     </main>
   </div>
